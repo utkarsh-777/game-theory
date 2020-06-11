f3ed4ac863a7cbc43a42242c4ca98f64fb47b065
Atishey Jain <atisheyjain1232@gmail.com>
2020-06-11 14:40:05 +0530

    added comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var tiles = Math.floor(Math.random() * 1000) + 1000;
+var tiles = Math.floor(Math.random() * 1000) + 1000; // total tiles left
 
 document.getElementById("tiles-val").innerHTML = tiles;
 
-var values = [];
+var values = []; // allowed withdrawls
 
 while (values.length < 4) {
   let x = Math.floor(Math.random() * 50) + 2;
@@ -13,16 +13,12 @@ while (values.length < 4) {
 values.sort(function (a, b) {
   return a - b;
 });
-// tiles = 30;
-// values = [1, 2, 3];
-var target_pos = [0];
 
-var flag = 0;
+var target_pos = [0]; // loosing position for opponent if they get on these position
 
-console.log(values);
-
+// setting loosing position
 for (var i = 1; i <= tiles; i++) {
-  flag = 0;
+  let flag = 0;
   for (var j = 0; j < values.length; j++) {
     if (values[j] > i) break;
     if (target_pos.includes(i - values[j])) {
@@ -32,21 +28,25 @@ for (var i = 1; i <= tiles; i++) {
   }
   if (!flag) target_pos.push(i);
 }
-console.log(target_pos);
 
 values.forEach((element, i) => {
   document.getElementById(`val-${i}`).innerHTML = values[i];
 });
 
-var turn = 1;
+var turn = 1; // current player turn
+
+// if turn = 1        --- player 1 turn
+// if turn = -1       --- player 2 turn
+
+// preparing for player 1 turn
 var elem = document.getElementById(`alert-p${turn}`);
 document.getElementById(`input-p${turn}`).focus();
-// elem.style.visibility = "visible";
 elem.classList.add("alert-success");
 elem.classList.remove("alert-danger");
 elem.innerHTML = "YOUR TURN";
 elem.classList.remove("invisible");
 
+//  this will make other player input dissable
 document.getElementById(`input-p-1`).setAttribute("readonly", "true");
 
 var callback = (event) => {
@@ -55,18 +55,21 @@ var callback = (event) => {
 
   let val = document.getElementById(`input-p${turn}`).value;
 
-  // console.log(val);
   val *= 1;
   if (tiles < val || !values.includes(val)) {
     document.getElementById(`input-p${turn}`).value = "";
+    // display invalid warning on invalid or empty move
     elem.classList.remove("alert-success");
     elem.classList.add("alert-danger");
     elem.innerHTML = "INVALID";
     elem.classList.remove("invisible");
   } else {
+    // if move accepted
+    // removing current player values
     document.getElementById(`input-p${turn}`).value = "";
     document.getElementById(`input-p${turn}`).setAttribute("readonly", "true");
     elem.classList.add("invisible");
+    // changing player
     turn *= -1;
     document.getElementById(`input-p${turn}`).removeAttribute("readonly");
     elem = document.getElementById(`alert-p${turn}`);
@@ -78,7 +81,7 @@ var callback = (event) => {
     tiles -= val;
     document.getElementById("tiles-val").innerHTML = tiles;
     if (tiles < values[0]) {
-      alert("you won the game");
+      alert(` ${turn === 1 ? "PLAYER 1" : "PLAYER 2"} WON THE GAME `);
       location.reload();
     }
   }
@@ -87,11 +90,12 @@ var callback = (event) => {
 document.getElementById(`form-p1`).addEventListener("submit", callback);
 document.getElementById(`form-p-1`).addEventListener("submit", callback);
 
-// window.onbeforeunload = function (event) {
-//   console.log("xxxxxx");
-//   return confirm("Confirm refresh");
-// };
+// give warning on reload/ refresh if any change accured
+window.onbeforeunload = function (event) {
+  return confirm("Confirm refresh");
+};
 
+// hint callback
 var hint = (event) => {
   if (event.target.id != `hint-p${turn}`) return;
   let t = [];
@@ -100,7 +104,6 @@ var hint = (event) => {
       t.push(values[i]);
     }
   }
-  console.log(t);
   let ans = 0;
   if (t.length === 0) ans = values[0];
   else ans = t[Math.floor(Math.random() * 1) + t.length - 1];
@@ -111,6 +114,6 @@ var hint = (event) => {
     document.getElementById(`hint-ans-p${turn}`).classList.add("invisible");
   }, 1000);
 };
-
+// hint eventlistener
 document.getElementById("hint-p1").addEventListener("click", hint);
 document.getElementById("hint-p-1").addEventListener("click", hint);
