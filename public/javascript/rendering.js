ed072a0b0ec2df5e393f0c54c653fad735ebadac
Atishey Jain <64005315+atisheyJain03@users.noreply.github.com>
2021-05-01 00:40:39 +0530

    Update rendering.js

--- public/javascript/rendering.js
+++ public/javascript/rendering.js
@@ -227,8 +227,7 @@ function start() {
           ? window.meta.product.variants[0].name
           : undefined;
       console.log(obj);
-      formElem = document.querySelector("#review-form");
-      new FormData(formElem);
+
       fetch(`http://127.0.0.1:8080/api/v1/review/createReview`, {
         method: "POST",
         headers: {
