ff704286e03916b3fa29907892ea56110a3c382e
Atishey Jain <64005315+atisheyJain03@users.noreply.github.com>
2021-05-01 00:20:57 +0530

    Update rendering.js

--- public/javascript/rendering.js
+++ public/javascript/rendering.js
@@ -230,8 +230,8 @@ function start() {
       fetch(`http://127.0.0.1:8080/api/v1/review/createReview`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
-          accept: "application/json",
+          "Content-Type": "multipart/form-data;",
+          // accept: "application/json",
         },
         body: JSON.stringify(obj),
       })
